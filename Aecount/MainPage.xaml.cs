2f18eaa4e1de81a2ae0ecc547e790248c91c098b
Johan Halin <johan@halin.me>
2013-03-31 19:41:58 +0300

    The counter is saved in isolated storage.

--- Aecount/MainPage.xaml.cs
+++ Aecount/MainPage.xaml.cs
@@ -11,19 +11,63 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Diagnostics;
+using System.IO.IsolatedStorage;
 
 namespace Aecount
 {
 	public partial class MainPage : PhoneApplicationPage
 	{
-		// Constructor
+		private IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+		private string CountKey = "count";
+		private string TitleKey = "title";
+		private string GoalKey = "goal";
+
+		private int Count
+		{
+			get
+			{
+				try
+				{
+					int count = (int)settings[CountKey];
+					return count;
+				}
+				catch (Exception ex)
+				{
+					this.Count = 0;
+					return 0;
+				}
+			}
+
+			set
+			{
+				Debug.WriteLine("Setting value to " + value);
+
+				if (settings.Contains(CountKey))
+				{
+					settings[CountKey] = value;
+				}
+				else
+				{
+					try
+					{
+						settings.Add(CountKey, value);
+					}
+					catch (Exception ex)
+					{
+						Debug.WriteLine("Unable to add count to settings: " + ex.Message);
+					}
+				}
+
+				settings.Save();
+			}
+		}
+
 		public MainPage()
 		{
 			InitializeComponent();
 
+			CountText.Text = Count.ToString();
 		}
-
-		private int Count = 0;
 
 		private void UpdateCountText(bool increment)
 		{
@@ -34,13 +78,13 @@ namespace Aecount
 
 		private void DecrementCount()
 		{
-			Count--;
+			this.Count--;
 			UpdateCountText(false);
 		}
 
 		private void IncrementCount()
 		{
-			Count++;
+			this.Count++;
 			UpdateCountText(true);
 		}
 
